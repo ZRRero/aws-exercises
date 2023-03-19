65a9c791ae0332339d7658ffced89162b08e367c
David Useche <david-use@hotmail.com>
2023-03-19 18:17:46 -0500

    Fixes on lambda file

--- exercises/computing/lambda/create_lambda_api_dynamo/solution/code/terraform/api.tf
+++ exercises/computing/lambda/create_lambda_api_dynamo/solution/code/terraform/api.tf
@@ -16,13 +16,13 @@ resource "aws_apigatewayv2_integration" "lambda_production_integration" {
   integration_type = "AWS_PROXY"
   connection_type = "INTERNET"
   integration_method = "POST"
-  integration_uri = aws_lambda_alias.lambda_alias.arn
+  integration_uri = aws_lambda_alias.lambda_alias.invoke_arn
   passthrough_behavior = "WHEN_NO_MATCH"
   payload_format_version = "2.0"
 }
 
 resource "aws_apigatewayv2_route" "lambda_production_route" {
   api_id    = aws_apigatewayv2_api.lambda_api.id
-  route_key = "POST /auth"
+  route_key = "ANY /events/{proxy+}"
   target = "integrations/${aws_apigatewayv2_integration.lambda_production_integration.id}"
 }
--- exercises/computing/lambda/create_lambda_api_dynamo/solution/code/terraform/lambda.tf
+++ exercises/computing/lambda/create_lambda_api_dynamo/solution/code/terraform/lambda.tf
@@ -1,6 +1,6 @@
 resource "aws_lambda_function" "lambda_function" {
   provider = aws.master_region
-  function_name = "lambda_function"
+  function_name = "lambda_function_versioned"
   role          = aws_iam_role.lambda_role.arn
   handler = "lambda_function.lambda_handler"
   runtime = "python3.9"
@@ -12,16 +12,16 @@ resource "aws_lambda_function" "lambda_function" {
 
 resource "aws_lambda_alias" "lambda_alias" {
   function_name = aws_lambda_function.lambda_function.function_name
-  role          = aws_iam_role.lambda_role.arn
-  function_version = "1"
+  function_version = aws_lambda_function.lambda_function.version
   name = "current"
 }
 
 resource "aws_lambda_permission" "lambda_api_permission" {
   provider = aws.master_region
-  statement_id = "AllowExecutionFromSNS"
+  statement_id = "AllowExecutionFromAPiGateway"
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.lambda_function.function_name
-  principal     = "sns.amazonaws.com"
-  source_arn = "${aws_apigatewayv2_api.lambda_api.execution_arn}/*"
+  principal     = "apigateway.amazonaws.com"
+  source_arn = "${aws_apigatewayv2_stage.lambda_production_stage.execution_arn}/ANY/events/{proxy+}"
+  qualifier = aws_lambda_alias.lambda_alias.name
 }
