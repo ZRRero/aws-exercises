cd8532d8518b3e27d507a5ef692a03536c4ad225
David Useche <david-use@hotmail.com>
2023-04-28 16:39:38 -0500

    Added rest of solution

--- exercises/database/memorydb/create_memorydb_database/solution/code/pulumi/__main__.py
+++ exercises/database/memorydb/create_memorydb_database/solution/code/pulumi/__main__.py
@@ -5,7 +5,7 @@ from pulumi_aws import ec2
 import pulumi_random as random
 
 vpcId = "id"
-subnetsIds = []
+subnetsIds = ["subnetA", "subnetB"]
 security_group = ec2.SecurityGroup("security-group", vpc_id=vpcId, ingress=[ec2.SecurityGroupIngressArgs(
     description="Public Redis security group",
     from_port=6379,
@@ -19,3 +19,11 @@ security_group = ec2.SecurityGroup("security-group", vpc_id=vpcId, ingress=[ec2.
     cidr_blocks=["0.0.0.0/0"],
 )])
 password = random.RandomPassword("password", length=16)
+user = memorydb.User("root", access_string="on ~* &* +@all", user_name="root",
+                     authentication_mode=memorydb.UserAuthenticationModeArgs(type="password",
+                                                                             passwords=[password.result]))
+acl = memorydb.Acl("acl", user_names=[user.user_name])
+subnet_group = memorydb.SubnetGroup("subnet-group", subnet_ids=subnetsIds)
+cluster = memorydb.Cluster("cluster", acl_name=acl.name, node_type="db.t4g.small", num_shards=2,
+                           num_replicas_per_shard=1, subnet_group_name=subnet_group.name,
+                           security_group_ids=[security_group.id])
